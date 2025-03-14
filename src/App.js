fa3353cad2febcc60e5abf3824f2d9d84985530c
Aira Mae Ogaco <airamaeogaco@gmail.com>
2025-03-14 20:59:45 +0000

    Updating comments
    Updating comments for App.js and GoalContext.js for future reference

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import Root from './components/Root';
-import { GoalProvider } from './context/GoalContext';
+import { GoalProvider } from './context/GoalContext'; /*context provider for managing goals*/
 import GoalForm from './components/GoalForm';
 import GoalItem from './components/GoalItem';
 import LearningList from './components/LearningList';
 
 import './App.css';
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path="/" element={<Root />}>
+const router = createBrowserRouter(createRoutesFromElements( /*createRoutesFromElements - helper function to define the routes*/
+  <Route path="/" element={<Root />}> {/* "/" will display the Root component */}
     <Route path="/goal-form" element={<GoalForm />} />
     <Route path="/goal-item" element={<GoalItem />} />
     <Route path="/learning-list" element={<LearningList />} />
@@ -18,9 +18,12 @@ const router = createBrowserRouter(createRoutesFromElements(
 
 function App() {
   return (
+    /* GoalProvider wraps the entire app, making GoalContext available 
+    to child components (e.g. GoalForm, LearningList)
+     to modify the goal state or needs access */
     <GoalProvider>
       <div>
-        <RouterProvider router={router} />
+        <RouterProvider router={router} /> {/* RouterProvider renders router & controls navigation between diff views */}
         <GoalForm />
         <LearningList />
         <GoalItem />
--- src/context/GoalContext.js
+++ src/context/GoalContext.js
@@ -1,17 +1,21 @@
-import { useState, createContext } from 'react';
+/* Managing state for goals & making it accessible 
+across the app using React's Context API */
+import { useState, createContext } from 'react'; /*useState for storing goals*/
 import { v4 as uuidv4 } from 'uuid';
 
-export const GoalContext = createContext()
+export const GoalContext = createContext() /*create context object for goals*/
 
 export function GoalProvider({children}) {
     const [goals, setGoals] = useState([]);
 
     const addGoal = (goal) => {
-        setGoals([...goals, { id: uuidv4(), text: goal }]);
+        setGoals([...goals, { id: uuidv4(), text: goal }]); /*generates a new goal with  a unique ID & user goal input to the list*/
     };
 
     return (
         <div>
+            {/* GoalContext.Provider makes the goals 
+            & addGoal function available to child components (e.g. App) via the context */}
             <GoalContext.Provider value={{ goals, addGoal }}>
                 {children}
             </GoalContext.Provider>
